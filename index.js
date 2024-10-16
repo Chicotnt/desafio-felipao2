fa663bd9de52e20986ec54f39a44872303733b1e
Chicotnt <chicoquantum@gmail.com>
2024-10-16 15:56:05 -0300

    novo commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let nome = "Wendell";
+let XP = 20000;
+let nivel;
+if (XP <= 1000) {
+    nivel = "Ferro";
+    console.log("O Herói de nome " + nome + " está no nivel de " + nivel);
+}
+else if (XP >= 1001 && XP <= 2000) {
+    nivel = "Bronze";
+    console.log("O Herói de nome " + nome + " está no nivel de " + nivel);
+}
+else if (XP >= 1001 && XP <= 2000) {
+    nivel = "Bronze";
+    console.log("O Herói de nome " + nome + " está no nivel de " + nivel);
+}
+else if (XP >= 2001 && XP <= 5000) {
+    nivel = "Prata";
+    console.log("O Herói de nome " + nome + " está no nivel de " + nivel);
+}
+else if (XP >= 5001 && XP <= 7000) {
+    nivel = "Ouro";
+    console.log("O Herói de nome " + nome + " está no nivel de " + nivel);
+}
+else if (XP >= 7001 && XP <= 8000) {
+    nivel = "Platina";
+    console.log("O Herói de nome " + nome + " está no nivel de " + nivel);
+}
+else if (XP >= 8001 && XP <= 9000) {
+    nivel = "Ascendente";
+    console.log("O Herói de nome " + nome + " está no nivel " + nivel);
+}
+else if (XP >= 9001 && XP <= 10000) {
+    nivel = "Imortal";
+    console.log("O Herói de nome " + nome + " está no nivel de " + nivel);
+}
+else {
+    nivel = "Radiante";
+    console.log("O Herói de nome " + nome + " está no nivel de " + nivel);
+}
